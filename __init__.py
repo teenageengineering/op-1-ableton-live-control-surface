25a2c47033b1ed6e2406b3b1916f64d4d0f4c0ac
Nuno Santos <nunosantos@imaginando.net>
2012-12-18 21:07:31 +0000

    V1.0

---
+++ __init__.py
@@ -0,0 +1,10 @@
+import Live
+    
+from OP1 import OP1
+
+def debug_print(message):
+    ' Special function for debug output '
+    print message
+    
+def create_instance(c_instance):
+    return OP1(c_instance)
