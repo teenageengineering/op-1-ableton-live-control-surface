f591fe7f8f3df0a0859faa163168cc0397463d7f
Nuno Santos <nunosantos@imaginando.net>
2014-07-08 11:28:43 +0100

    Updated license

--- OP1ModeSelectorComponent.py
+++ OP1ModeSelectorComponent.py
@@ -1,3 +1,22 @@
+/*******************************************************************
+**
+** Copyright (C) 2012 Imaginando, Lda & Teenage Engineering AB
+** 
+** This program is free software; you can redistribute it and/or
+** modify it under the terms of the GNU General Public License
+** as published by the Free Software Foundation; either version 2
+** of the License, or any later version.
+** 
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** For more information about this license please consult the
+** following webpage: http://www.gnu.org/licenses/gpl-2.0.html
+**
+*******************************************************************/
+
 import Live;
 
 from consts import *
